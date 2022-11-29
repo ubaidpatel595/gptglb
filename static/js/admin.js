76ad4453598f4f007410a3b26c2061a57abb037a
ubaidpatel595 <fakeuser595@gmail.com>
2022-11-29 15:56:46 +0530

    Added Admin

---
+++ static/js/admin.js
@@ -0,0 +1,14 @@
+opt_list = ["upload","view","delete"];
+function show(elem,hide){
+    let opt = document.getElementById(elem);
+    opt.style="display:block";
+    for(x in hide){
+        let opt = document.getElementById(hide[x]);
+        opt.style="display:none";
+    }
+}
+function show_action(act,type){
+    let elem = document.getElementById("action");
+    elem.style='display:block';
+    elem.src=act+type+'.html';
+}
